72124fc650d71ed268e76e85e91bb974f7cbe527
Angelpro09_Dev <aloplop090708@gmail.com>
2026-07-24 19:08:37 +0000

    sapphire: Add privapp permissions and allowlist for MIUIGalleryGlobal
    * Define LOCAL_PATH in device.mk to ensure configs are copied properly.
    * Fix XML syntax and grant required privileged permissions for com.miui.gallery.
    * Add com.miui.gallery to hidden-api package allowlist.

--- device.mk
+++ device.mk
@@ -4,6 +4,8 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+LOCAL_PATH := device/xiaomi/miuicamera-sapphire
+
 # Inherit from the proprietary version
 $(call inherit-product, vendor/xiaomi/miuicamera-sapphire/miuicamera-sapphire-vendor.mk)
 
